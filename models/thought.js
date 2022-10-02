8816faa107ca73bf0668c90769185ece2a390d60
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-01 20:56:30 -0700

    set up reaction model

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const ReactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
 const ThoughtSchema = new Schema(
@@ -19,7 +19,7 @@ const ThoughtSchema = new Schema(
             type: String,
             required: true
         },
-        reactions: [reactionSchema]
+        reactions: [ReactionSchema]
     },
     {
         toJSON: {
