33a3f2c0f9f310dc98ba784599e372630ec6cb7e
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-01 20:46:37 -0700

    set up thought.js model

--- models/user.js
+++ models/user.js
@@ -30,8 +30,7 @@ const UserSchema = new Schema(
     },
     {
         toJSON: {
-            virtuals: true,
-            getters: true
+            virtuals: true
         },
         id: false
     }
