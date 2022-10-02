699bad2c5cb02a5ffe0a6c95077808ee0a15888b
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-01 21:05:06 -0700

    set up user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const { getAllUsers, getUserById, createUser, updateUserById, deleteUserById } = require('../../controllers/user-controller');
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser)
+
+// /api/users/:userId
+router
+    .route('/:userId')
+    .get(getUserById)
+    .put(updateUserById)
+    .delete(deleteUserById);
+
+module.exports = router;
