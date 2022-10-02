9119a29077f7970846d956a2974088d9c6a4b5e5
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-01 23:36:18 -0700

    setup thought controller - setup Insomnia for testing

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,37 +3,90 @@ const { Thought, User } = require('../models');
 const thoughtController = {
     // get all thoughts
     getAllThoughts(req, res) {
-
+        Thought.find()
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
     }, 
     
     // get a single thought by id
     getThought(req, res) {
-
+        Thought.findOne({ _id: req.params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
     }, 
     
     // create a new thought and push id to associated user thoughts
     createThought (req, res) {
-
+        Thought.create({ $push: { _id: req.params.userId }})
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err)); 
     },
     
     // update a thought by id
     updateThought(req, res) {
-
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, {new: true})
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
     },
     
     // remove a thought by its id
     removeThought(req, res) {
-
+        Thought.findByIdAndRemove({ _id: req.params.thoughtId })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
     },
     
     // create a reaction stored in a single thought's reactions array field
     addReaction(req, res) {
-
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { new: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id! '});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
     },
     
     // pull and remove a reaction by the reactionId value
     removeReaction(req, res) {
-
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { new: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id! '});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
     },
 };
 
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,7 +14,7 @@ const userController = {
     
     // get a single user by id with populated thought and friend data
     getUser(req, res) {
-        User.findOne({ _id: params.id })
+        User.findOne({ _id: req.params.id })
             .populate('thought')
             .populate('friend')
             .select('-__v')
@@ -38,7 +38,7 @@ const userController = {
     
     // update user by id
     updateUser(req, res) {
-        User.findOneAndUpdate({ _id: params.id }, {
+        User.findOneAndUpdate({ _id: req.params.id }, {
             new: true,
             runValidators: true
         }
@@ -56,7 +56,7 @@ const userController = {
     
     // delete user by id
     deleteUser(req, res) {
-        User.findOneAndDelete({ _id: params.userId })
+        User.findOneAndDelete({ _id: req.params.userId })
             .then(dbUserData => {
                 console.log(dbUserData);
                 if (!dbUserData) {
@@ -73,7 +73,7 @@ const userController = {
     
     // add a new friend (:friendId) to a user's friend list (:userId)
     addFriend(req, res) {
-        User.findOneAndUpdate({ _id: params.userId }, {friends: params.friendsId})
+        User.findOneAndUpdate({ _id: req.params.userId }, {$push: {friends: req.params.friendsId}})
             .then(dbUserData => {
                 console.log(dbUserData);
                 if (!dbUserData) {
@@ -86,7 +86,7 @@ const userController = {
     
     // remove a friend (:friendId) from a user's friend list (:userId)
     deleteFriend(req, res) {
-        User.findOneAndUpdate({ _id: params.userId }, {friends: params.friendsId})
+        User.findOneAndUpdate({ _id: params.userId }, {$pull: {friends: req.params.friendsId}})
             .then(dbUserData => {
                 console.log(dbUserData);
                 if (!dbUserData) {
